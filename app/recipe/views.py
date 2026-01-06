b1bf407a46ac852f76dd67ee3d28c2b0b029d4ac
Michael Hund <mhund@zerofox.com>
2026-01-06 12:29:24 -0300

    Add Tag model, serializer, and API endpoints for managing tags, including tests for tag retrieval and user-specific access.

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -2,8 +2,14 @@
 Views for the recipe API.
 """
 
-from core.models import Recipe
-from rest_framework import viewsets
+from core.models import (
+    Recipe,
+    Tag,
+)
+from rest_framework import (
+    mixins,
+    viewsets,
+)
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
@@ -30,3 +36,16 @@ class RecipeViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         """Create a new recipe."""
         serializer.save(user=self.request.user)
+
+
+class TagViewSet(mixins.ListModelMixin,
+                 viewsets.GenericViewSet):
+    """Manage tags in the database."""
+    serializer_class = serializers.TagSerializer
+    queryset = Tag.objects.all()
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        """Retrieve tags for authenticated user."""
+        return self.queryset.filter(user=self.request.user).order_by('-name')
