7217b58ee8414eec8642f50f180c07f41d3846b9
Michael Hund <mhund@zerofox.com>
2026-01-06 12:38:14 -0300

    Enhance TagViewSet to support update and delete operations, and add corresponding tests for tag updates and deletions.

--- app/recipe/tests/test_tags_api.py
+++ app/recipe/tests/test_tags_api.py
@@ -14,6 +14,11 @@ from recipe.serializers import TagSerializer
 TAGS_URL = reverse('recipe:tag-list')
 
 
+def detail_url(tag_id):
+    """Create and return a tag detail URL."""
+    return reverse('recipe:tag-detail', args=[tag_id])
+
+
 def create_user(email='test@example.com', password='testpass123'):
     """Create and return a test user."""
     return get_user_model().objects.create_user(email, password)
@@ -60,3 +65,24 @@ class PrivateTagsApiTests(TestCase):
         self.assertEqual(len(res.data), 1)
         self.assertEqual(res.data[0]['name'], tag.name)
         self.assertEqual(res.data[0]['id'], tag.id)
+
+    def test_update_tag(self):
+        """Test updating a tag."""
+        tag = Tag.objects.create(user=self.user, name='Tag5')
+
+        payload = {'name': 'New tag name'}
+        url = detail_url(tag.id)
+        res = self.client.patch(url, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        tag.refresh_from_db()
+        self.assertEqual(tag.name, payload['name'])
+        self.assertEqual(tag.user, self.user)
+
+    def test_delete_tag(self):
+        """Test deleting a tag."""
+        tag = Tag.objects.create(user=self.user, name='Tag6')
+        url = detail_url(tag.id)
+        res = self.client.delete(url)
+        self.assertEqual(res.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertFalse(Tag.objects.filter(id=tag.id).exists())
