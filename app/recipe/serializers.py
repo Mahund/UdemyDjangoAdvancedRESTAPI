d3ca9ef2708476dc112464dc28b692027a3e6525
Michael Hund <mhund@zerofox.com>
2026-01-06 11:37:25 -0300

    Add recipe management functionality with model, serializers, views, and API endpoints, including tests for recipe creation and retrieval.

---
+++ app/recipe/serializers.py
@@ -0,0 +1,22 @@
+"""
+Serializers for the recipe API View.
+"""
+
+from core.models import Recipe
+from rest_framework import serializers
+
+
+class RecipeSerializer(serializers.ModelSerializer):
+    """Serializer for the recipe object."""
+
+    class Meta:
+        model = Recipe
+        fields = ['id', 'title', 'time_minutes', 'price', 'link']
+        read_only_fields = ['id']
+
+
+class RecipeDetailSerializer(RecipeSerializer):
+    """Serializer for the recipe detail object."""
+
+    class Meta(RecipeSerializer.Meta):
+        fields = RecipeSerializer.Meta.fields + ['description']
